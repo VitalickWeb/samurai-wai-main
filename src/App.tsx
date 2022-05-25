c6146330bb3ac4839652b6d13e3f1863dec184ad
Vitalick <miraigeweb@gmail.com>
2022-05-25 23:05:45 +0300

    first commit samurai

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Tehnologies from './Technologies';
+import Header from './Header';
+
+const App = () => {
+  return (
+      <div>
+          <Header />
+          <Tehnologies />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">html</a>
+            <a href="#">css</a>
+            <a href="#">js</a>
+            <a href="#">react</a>
+        </div>
+    );
+}
+
+export default Header;
+
+
